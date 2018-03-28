af4e0b5ed177e1255c7b8c09c6530a7cfa6deb48
RAMYAMP <RAMYAMP@RAMYAMP-PC>
2018-03-27 22:34:35 -0700

    Initial project Multiple choice

---
+++ ConsoleApp3/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoice
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string Grade { get; set; }
+        public decimal Result { get; set; }
+        public string Subject { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
