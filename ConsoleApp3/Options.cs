5ab9bee9e112f3e14b6b391e4136280d17d6485a
RAMYAMP <RAMYAMP@RAMYAMP-PC>
2018-03-30 19:15:22 -0700

    Defined classes

---
+++ ConsoleApp3/Options.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoice
+{
+    class Options
+    {
+        public string OptionsText { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
---
+++ ConsoleApp3/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoice
+{
+    class Question
+    {
+        public int QuestionNo { get; set; }
+        public string QuestionText { get; set; }
+        public int Options { get; set; }//list
+    }
+}
